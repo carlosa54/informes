75ed31fb7a4471441897e1f37a4373cd9e763b69
carlosa54 <carlos_arg5@yahoo.com>
2015-10-05 15:25:42 -0400

    Changed questions model

--- informes/questions/models.py
+++ informes/questions/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from ..utils.models import BaseModel
-from django.core.validators import MinValueValidator, MaxValueValidator
 from ..persons.models import Person
 # Create your models here.
 
 class Question(BaseModel):
 	question_text = models.CharField(max_length=200)
-	
+
 	def __unicode__(self):
 		return self.question_text	
